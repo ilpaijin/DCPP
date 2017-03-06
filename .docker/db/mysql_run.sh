5b1c4b5d1051b2c2510e19498300abf4ca041370
ilpaijin <ilpaijin@gmail.com>
2017-03-06 17:23:07 +0100

    using env var

--- .docker/db/mysql_run.sh
+++ .docker/db/mysql_run.sh
@@ -7,12 +7,11 @@ mysqld_safe &
 
 sleep 10
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS docker_compose_pet_project"
-mysql -u root -e "CREATE USER 'paolo'@'%' IDENTIFIED BY 'pwd';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'paolo'@'%';"
-
-mysql -u root docker_compose_pet_project < ./dump.sql
-
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES"
+
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < dump.sql
 
 fg
